94d9ecd077183d41572d78e48cae9e6ad392bb44
Alexey Svistunov <xdayx53@gmail.com>
2023-08-26 11:39:07 +0300

    added full sections

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    
+      navigationDisabledClass: true,
+
+    },
+
+    slidesPerView: 3,
+    spaceBetween: 30,
+    // loop: true,
+
+  });
