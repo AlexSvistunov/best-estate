0013ab76a097f1c69193afc0c906b8492e91d764
AlexSvistunov <112976775+AlexSvistunov@users.noreply.github.com>
2023-10-07 21:29:38 +0300

    add white theme and filters

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ const swiper = new Swiper('.swiper', {
     breakpoints: {
         
         360: {
-            slidesPerView: 1.15,
+            slidesPerView: 1,
         },
 
         768: {
@@ -29,56 +29,3 @@ const swiper = new Swiper('.swiper', {
     },
 
   });
-
-const projButtons = document.querySelectorAll('.projects__btn');
-projButtons.forEach(projButton => {
-
-  projButton.addEventListener('click', (e) => {
-    const dataBtn = projButton.getAttribute('data-filter');
-    console.log(dataBtn);
-    projButtons.forEach(projBtn => {
-      projBtn.classList.remove('active');
-    });
-    const self = e.currentTarget;
-    self.classList.add('active');
-
-
-    const projItems = document.querySelectorAll('.projects__item');
-    projItems.forEach(item => {
-      const dataItem = item.getAttribute('data-target');
-
-      if(dataBtn === "all" || dataBtn === dataItem) {
-        item.style.display = "block";
-      }
-      else {
-        item.style.display = "none";
-      }
-  
-    });
-
-    
-  });
-
-
-
-});
-
-
-const showMore = document.querySelector('.projects__show-link');
-const productsLength = document.querySelectorAll('.projects__item').length;
-let currentVisibleElements = 4;
-showMore.addEventListener('click', () => {
-  currentVisibleElements += 2;
-  const array = Array.from(document.querySelectorAll('.projects__item'));
-  const visItems = array.slice(currentVisibleElements - 2, currentVisibleElements);
-
-  visItems.forEach(el => {
-    el.classList.add('visible');
-  });
-
-  if(currentVisibleElements === productsLength) {
-    showMore.style.display = "none";
-  }
-    
-
-});
